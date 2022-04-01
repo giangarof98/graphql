3f9b49b9d93ca6c68f0576bd4ec13a48fb64c964
Gianmarco G <giangarof98@gmail.com>
2022-04-01 10:51:37 -0400

    find product by id

--- products/products.resolvers.js
+++ products/products.resolvers.js
@@ -3,6 +3,12 @@ module.exports = {
     Query: {
         products: () => {
             return productsModel.getAllProducts()
+        },
+        productsByPrice: (_, args) => {
+            return productsModel.getProductsByPrice(args.min, args.max);
+        },
+        product: (_, args) => {
+            return productsModel.getProductsById(args.id);
         }
     }
 }
